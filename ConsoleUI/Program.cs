3bb0019364f37e9f94ba5e8257ea16b97ba92207
Rıdvan Abay <ridvanabay@hotmail.com>
2021-04-22 23:45:46 +0300

    7.Gün - Ödev 1 #added - NTier layer #added - Car Entity #added - InMemory Test Data

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Car car1 = new Car
+            {
+                Id=1,
+                BrandId = 1,
+                ColorId = 1,
+                DailyPrice = 100,
+                Description = "Mercedes Benz",
+                ModelYear = 2000
+            };
+
+            Car car2 = new Car
+            {
+                Id = 2,
+                BrandId = 2,
+                ColorId = 2,
+                DailyPrice = 200,
+                Description = "BMW",
+                ModelYear = 2005
+            };
+
+            Car car3 = new Car
+            {
+                Id = 3,
+                BrandId = 3,
+                ColorId = 3,
+                DailyPrice = 300,
+                Description = "BMW",
+                ModelYear = 2010
+            };
+            TestContext.Add(car1);
+            TestContext.Add(car2);
+            TestContext.Add(car3);
+            TestContext.Delete(car3);
+            car1.ModelYear = 2021;
+            //TestContext.Update(car1);
+            Console.WriteLine(TestContext.GetById(1).ModelYear);
+            Console.WriteLine(TestContext.GetById(2).Description);
+            foreach (var car in TestContext.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine(TestContext.GetAll().Count);
+
+           
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/TestContext.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Concrete;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public static class TestContext
+    {
+        static List<Car> _cars = new List<Car>();
+      
+       public static List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public static Car GetById(int carId)
+        {
+            return _cars.FirstOrDefault(c=>c.Id==carId);
+        }
+
+        public static void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+        public static void Update(Car car)
+        {
+            Car updatedCar = _cars.FirstOrDefault(c => c.Id == car.Id);
+            updatedCar.BrandId = car.BrandId;
+            updatedCar.ColorId = car.ColorId;
+            updatedCar.DailyPrice = car.DailyPrice;
+            updatedCar.Description = car.Description;
+            updatedCar.ModelYear = car.ModelYear;
+        }
+        public static void Delete(Car car)
+        {
+            _cars.Remove(car);
+        }
+    }
+}
