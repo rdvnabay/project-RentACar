2520cf112f9dba37c121c4ffcbe88364ae4934e7
Rıdvan <ridvanabay@gmail.com>
2021-05-02 22:55:53 +0300

    #template editing - Brand-Add

---
+++ WebUIAspNetMvcCore/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUIAspNetMvcCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private IBrandService _brandService;
+        public BrandController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Brand brand)
+        {
+            _brandService.Add(brand);
+            return View();
+        }
+    }
+}
