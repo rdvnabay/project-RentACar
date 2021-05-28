dc39a534696f112a5f311e33574869ca070041a9
Rıdvan <ridvanabay@gmail.com>
2021-05-29 01:51:26 +0300

    Added CQRS Command for Translate Delete

---
+++ Libraries/Business/Handlers/Translates/Commands/DeleteTranslateCommand.cs
@@ -0,0 +1,34 @@
+﻿using Business.Constants;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Translates.Commands
+{
+    public class DeleteTranslateCommand : IRequest<IResult>
+    {
+        public int Id { get; set; }
+
+        public class DeleteTranslateCommandHandler : IRequestHandler<DeleteTranslateCommand, IResult>
+        {
+            private readonly ITranslateDal _translateDal;
+            private readonly IMediator _mediator;
+
+            public DeleteTranslateCommandHandler(ITranslateDal translateDal, IMediator mediator)
+            {
+                _translateDal = translateDal;
+                _mediator = mediator;
+            }
+
+            public async Task<IResult> Handle(DeleteTranslateCommand request, CancellationToken cancellationToken)
+            {
+                var translateToDelete = _translateDal.Get(p => p.Id == request.Id);
+                _translateDal.Delete(translateToDelete);
+                return new SuccessResult(Messages.Deleted);
+            }
+        }
+    }
+}
