74fbc351487440d643622a500738d45ac361487c
Rıdvan <ridvanabay@gmail.com>
2021-05-29 02:07:26 +0300

    Added CQRS Query for Translate GetTranslateQuery

---
+++ Libraries/Business/Handlers/Translates/Queries/GetTranslateQuery.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Translates.Queries
+{
+    public class GetTranslateQuery : IRequest<IDataResult<Translate>>
+    {
+        public int Id { get; set; }
+
+        public class GetTranslateQueryHandler : IRequestHandler<GetTranslateQuery, IDataResult<Translate>>
+        {
+            private readonly ITranslateDal _translateDal;
+            private readonly IMediator _mediator;
+
+            public GetTranslateQueryHandler(ITranslateDal translateDal, IMediator mediator)
+            {
+                _translateDal = translateDal;
+                _mediator = mediator;
+            }
+            
+            public async Task<IDataResult<Translate>> Handle(GetTranslateQuery request, CancellationToken cancellationToken)
+            {
+                var translate =  _translateDal.Get(p => p.Id == request.Id);
+                return new SuccessDataResult<Translate>(translate);
+            }
+        }
+    }
+}
