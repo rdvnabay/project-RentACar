f12e60e00b4447108ce736a258d68944f1db3084
Rıdvan <ridvanabay@gmail.com>
2021-05-29 01:29:00 +0300

    Added CQRS Query for Language GetAll

---
+++ Libraries/Business/Handlers/Languages/Queries/GetLanguagesQuery.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Languages.Queries
+{
+    public class GetLanguagesQuery:IRequest<IDataResult<IEnumerable<Language>>>
+    {
+        public class GetLanguagesQueryHandler : IRequestHandler<GetLanguagesQuery, IDataResult<IEnumerable<Language>>>
+        {
+            private readonly ILanguageDal _languageDal;
+            private readonly IMediator _mediator;
+
+            public GetLanguagesQueryHandler(ILanguageDal languageDal, IMediator mediator)
+            {
+                _languageDal = languageDal;
+                _mediator = mediator;
+            }
+            public async Task<IDataResult<IEnumerable<Language>>> Handle(GetLanguagesQuery request, CancellationToken cancellationToken)
+            {
+                var languages = _languageDal.GetAll();
+                return new SuccessDataResult<IEnumerable<Language>>(languages);
+            }
+        }
+    }
+}
