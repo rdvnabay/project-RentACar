e9c442a1d214fa96fcda2ac49c79912b542667bc
Rıdvan <ridvanabay@gmail.com>
2021-05-30 20:55:36 +0300

    Added CQRS Command for Brand Create

---
+++ Libraries/Business/Handlers/Brands/Commands/CreateBrandCommand.cs
@@ -0,0 +1,44 @@
+﻿using Business.Constants;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Brands.Commands
+{
+    public class CreateBrandCommand : IRequest<IResult>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, IResult>
+        {
+            private readonly IBrandDal _brandDal;
+            private readonly IMediator _mediator;
+            public CreateBrandCommandHandler(IBrandDal brandDal, IMediator mediator)
+            {
+                _brandDal = brandDal;
+                _mediator = mediator;
+            }
+            public async Task<IResult> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                var isThereBrandRecord = _brandDal.GetAll().Any(x => x.Name == request.Name);
+                if (isThereBrandRecord)
+                {
+                    return new ErrorResult(Messages.NameAlreadyExist);
+                }
+                var addedBrand = new Brand
+                {
+                    Name = request.Name
+                };
+                _brandDal.Add(addedBrand);
+                return new SuccessResult(Messages.Added);
+            }
+        }
+    }
+}
+
+           
