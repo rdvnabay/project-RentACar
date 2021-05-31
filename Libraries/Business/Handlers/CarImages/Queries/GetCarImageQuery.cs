451c90388712b3506f10953a69dc11e919e75913
Rıdvan <ridvanabay@gmail.com>
2021-05-31 14:11:56 +0300

    Added CQRS Query for CarImage - Get and GetAll

---
+++ Libraries/Business/Handlers/CarImages/Queries/GetCarImageQuery.cs
@@ -0,0 +1,31 @@
+﻿using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.CarImages.Queries
+{
+    public class GetCarImageQuery:IRequest<IDataResult<CarImage>>
+    {
+        public int Id { get; set; }
+
+        public class GetCarImageQueryHandler : IRequestHandler<GetCarImageQuery, IDataResult<CarImage>>
+        {
+            private readonly IMediator _mediator;
+            private readonly ICarImageDal _carImageDal;
+            public GetCarImageQueryHandler(IMediator mediator, ICarImageDal carImageDal)
+            {
+                _mediator = mediator;
+                _carImageDal = carImageDal;
+            }
+            public async Task<IDataResult<CarImage>> Handle(GetCarImageQuery request, CancellationToken cancellationToken)
+            {
+                var carImage = await _carImageDal.GetAsync(x => x.Id == request.Id);
+                return new SuccessDataResult<CarImage>(carImage);
+            }
+        }
+    }
+}
---
+++ Libraries/Business/Handlers/CarImages/Queries/GetCarImagesQuery.cs
@@ -0,0 +1,36 @@
+﻿using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.CarImages.Queries
+{
+    public class GetCarImagesQuery:IRequest<IDataResult<IEnumerable<CarImage>>>
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime Date { get; set; }
+
+        public class GetCarImagesQueryHandler : IRequestHandler<GetCarImagesQuery, IDataResult<IEnumerable<CarImage>>>
+        {
+            private readonly IMediator _mediator;
+            private readonly ICarImageDal _carImageDal;
+            public GetCarImagesQueryHandler(IMediator mediator, ICarImageDal carImageDal)
+            {
+                _mediator = mediator;
+                _carImageDal = carImageDal;
+            }
+            public async Task<IDataResult<IEnumerable<CarImage>>> Handle(GetCarImagesQuery request, CancellationToken cancellationToken)
+            {
+                var carImages = await _carImageDal.GetAllAsync();
+                return new SuccessDataResult<IEnumerable<CarImage>>(carImages);
+            }
+        }
+    }
+}
