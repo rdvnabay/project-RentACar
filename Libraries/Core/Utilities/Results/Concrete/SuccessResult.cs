1bb0ec97352e8d4c451ab409d148c6395ab39992
Rıdvan <ridvanabay@gmail.com>
2021-05-31 23:05:40 +0300

    Updated SuccessResult Deleted using ErrorResult

--- Libraries/Core/Utilities/Results/Concrete/SuccessResult.cs
+++ Libraries/Core/Utilities/Results/Concrete/SuccessResult.cs
@@ -1,14 +1,8 @@
 ﻿namespace Core.Utilities.Results.Concrete
 {
-    public class SuccessResult:Result
+    public class SuccessResult : Result
     {
-        public SuccessResult(string message):base(true)
-        {
-
-        }
-        public SuccessResult():base(true)
-        {
-
-        }
+        public SuccessResult(string message) : base(true, message) { }
+        public SuccessResult() : base(true) { }
     }
 }
