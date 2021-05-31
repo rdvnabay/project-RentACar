56dc837f33ca5475023533ababd0009ce53ddfea
Rıdvan <ridvanabay@gmail.com>
2021-05-31 14:00:44 +0300

    Added CQRS Query for Color - Get and GetAll

---
+++ Libraries/Business/Handlers/Colors/Queries/GetColorQuery.cs
@@ -0,0 +1,31 @@
+﻿using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Brands.Queries
+{
+    public class GetColorQuery : IRequest<IDataResult<Color>>
+    {
+        public int ColorId { get; set; }
+
+        public class GetColorQueryHandler : IRequestHandler<GetColorQuery, IDataResult<Color>>
+        {
+            private readonly IMediator _mediator;
+            private readonly IColorDal _colorDal;
+            public GetColorQueryHandler(IMediator mediator, IColorDal colorDal)
+            {
+                _mediator = mediator;
+                _colorDal = colorDal;
+            }
+            public async Task<IDataResult<Color>> Handle(GetColorQuery request, CancellationToken cancellationToken)
+            {
+                var color = await _colorDal.GetAsync(x => x.Id == request.ColorId);
+                return new SuccessDataResult<Color>(color);
+            }
+        }
+    }
+}
---
+++ Libraries/Business/Handlers/Colors/Queries/GetColorsQuery.cs
@@ -0,0 +1,33 @@
+﻿using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Colors.Queries
+{
+    public  class GetColorsQuery:IRequest<IDataResult<IEnumerable<Color>>>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public class GetColorsQueryHandler : IRequestHandler<GetColorsQuery, IDataResult<IEnumerable<Color>>>
+        {
+            private readonly IMediator _mediator;
+            private readonly IColorDal _colorDal;
+            public GetColorsQueryHandler(IMediator mediator, IColorDal colorDal)
+            {
+                _mediator = mediator;
+                _colorDal = colorDal;
+            }
+            public async Task<IDataResult<IEnumerable<Color>>> Handle(GetColorsQuery request, CancellationToken cancellationToken)
+            {
+                var colors = await _colorDal.GetAllAsync();
+                return new SuccessDataResult<IEnumerable<Color>>(colors);
+            }
+        }
+    }
+}
