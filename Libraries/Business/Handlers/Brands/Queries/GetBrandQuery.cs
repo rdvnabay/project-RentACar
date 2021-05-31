27579f6eb9fb0d61475bbae9adce2f28ffad8d78
Rıdvan <ridvanabay@gmail.com>
2021-05-31 12:28:28 +0300

    Added CQRS Query for Brand - Get and GetAll

---
+++ Libraries/Business/Handlers/Brands/Queries/GetBrandQuery.cs
@@ -0,0 +1,31 @@
+﻿using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Brands.Queries
+{
+    public class GetBrandQuery:IRequest<IDataResult<Brand>>
+    {
+        public int BrandId { get; set; }
+
+        public class GetBrandQueryHandler : IRequestHandler<GetBrandQuery, IDataResult<Brand>>
+        {
+            private readonly IMediator _mediator;
+            private readonly IBrandDal _brandDal;
+            public GetBrandQueryHandler(IMediator mediator, IBrandDal brandDal)
+            {
+                _mediator = mediator;
+                _brandDal = brandDal;
+            }
+            public async Task<IDataResult<Brand>> Handle(GetBrandQuery request, CancellationToken cancellationToken)
+            {
+                var brand = await _brandDal.GetAsync(x => x.Id == request.BrandId);
+                return new SuccessDataResult<Brand>(brand);
+            }
+        }
+    }
+}
---
+++ Libraries/Business/Handlers/Brands/Queries/GetBrandsQuery.cs
@@ -0,0 +1,34 @@
+﻿using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Brands.Queries
+{
+    public class GetBrandsQuery:IRequest<IDataResult<IEnumerable<Brand>>>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public class GetBrandsQueryHandler : IRequestHandler<GetBrandsQuery, IDataResult<IEnumerable<Brand>>>
+        {
+            private readonly IMediator _mediator;
+            private readonly IBrandDal _brandDal;
+
+            public GetBrandsQueryHandler(IMediator mediator, IBrandDal brandDal)
+            {
+                _mediator = mediator;
+                _brandDal = brandDal;
+            }
+            public async Task<IDataResult<IEnumerable<Brand>>> Handle(GetBrandsQuery request, CancellationToken cancellationToken)
+            {
+                var brands = await _brandDal.GetAllAsync();
+                return new SuccessDataResult<IEnumerable<Brand>>(brands);
+            }
+        }
+    }
+}
