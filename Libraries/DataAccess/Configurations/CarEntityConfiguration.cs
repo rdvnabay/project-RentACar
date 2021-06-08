509200a883c79c7c540d6eeaa58e0a4139f884fb
Rıdvan <ridvanabay@gmail.com>
2021-06-08 20:23:33 +0300

    Added FluentAPI Configuration for Car Entity

---
+++ Libraries/DataAccess/Configurations/CarEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations
+{
+    public class CarEntityConfiguration : IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+        }
+    }
+}
