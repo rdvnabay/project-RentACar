6cb918e2270cd3c4a49c775be220f36f86422127
Rıdvan <ridvanabay@gmail.com>
2021-06-08 20:24:16 +0300

    Added FluentAPI Configuration for Color Entity

---
+++ Libraries/DataAccess/Configurations/ColorEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations
+{
+    public class ColorEntityConfiguration : IEntityTypeConfiguration<Color>
+    {
+        public void Configure(EntityTypeBuilder<Color> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+        }
+    }
+}
