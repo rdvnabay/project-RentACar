c65b4d6167cf8cd6843c51e9622e6a3366963d19
Rıdvan <ridvanabay@gmail.com>
2021-06-08 20:24:02 +0300

    Added FluentAPI Configuration for Rental Entity

---
+++ Libraries/DataAccess/Configurations/RentalEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace DataAccess.Configurations
+{
+    public class RentalEntityConfiguration : IEntityTypeConfiguration<Rental>
+    {
+        public void Configure(EntityTypeBuilder<Rental> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
