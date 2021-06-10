23d459d6010e8cd20c7b198b8130a6faf2e85351
Rıdvan <ridvanabay@gmail.com>
2021-06-10 21:29:30 +0300

    Added MVC View for CarImage (Add and Edit)

---
+++ Presentations/WebUIAspNetMvcCore/Areas/AdminPanel/Controllers/CarImageController.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using Entities.Dtos.CarImage;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUIAspNetMvcCore.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class CarImageController : Controller
+    {
+        private ICarImageService _carImageService;
+
+        public CarImageController(ICarImageService carImageService)
+        {
+            _carImageService = carImageService;
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(CarImageAddDto carImageAddDto,IFormFile[] files)
+        {
+            var result = _carImageService.Add(carImageAddDto, files);
+            return result.Success
+               ? Ok(result)
+               : BadRequest(result);
+        }
+        public IActionResult Edit(int carImageId)
+        {
+            var carImage = _carImageService.GetByCarId(carImageId);
+            return View(carImage);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CarImageUpdateDto carImageUpdateDto, IFormFile[] files)
+        {
+            var result = _carImageService.Update(carImageUpdateDto, files);
+            return result.Success 
+                ? Ok(result) 
+                : BadRequest();
+        }
+    }
+}
