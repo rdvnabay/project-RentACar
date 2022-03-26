1f9e2a91e19b4d2624b285b6063473f47f1dd007
rdvnabay <ridvanabay@gmail.com>
2022-03-26 14:08:19 +0300

    Move Entities layer under the src folder

---
+++ Libraries/DataAccess/Context/RentACarDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+
+namespace DataAccess.Context
+{
+    public class RentACarDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public RentACarDbContext()
+        {
+
+        }
+        public RentACarDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                base.OnConfiguring(optionsBuilder.UseSqlServer(_configuration.GetConnectionString("RentACarDbContext")));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarImage> CarImages { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+
+
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Translate> Translates { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    }
+}
