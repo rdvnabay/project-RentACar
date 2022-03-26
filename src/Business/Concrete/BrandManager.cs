c9ab970a80b3fa82ccc8697e0f732e7dc1baf265
rdvnabay <ridvanabay@gmail.com>
2022-03-26 15:06:43 +0300

    Delete Console app

--- src/Business/Concrete/BrandManager.cs
+++ src/Business/Concrete/BrandManager.cs
@@ -3,9 +3,11 @@ using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Business;
 using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.Dtos.Brand;
@@ -17,11 +19,11 @@ namespace Business.Concrete
 {
     public class BrandManager : IBrandService
     {
-        IBrandRepository _brandDal;
+        IBrandRepository _brandRepository;
         IMapper _mapper;
-        public BrandManager(IBrandRepository brandDal, IMapper mapper)
+        public BrandManager(IBrandRepository brandRepository, IMapper mapper)
         {
-            _brandDal = brandDal;
+            _brandRepository = brandRepository;
             _mapper = mapper;
         }
 
@@ -37,45 +39,45 @@ namespace Business.Concrete
             {
                 return result;
             }
-            _brandDal.Add(brand);
+            _brandRepository.Add(brand);
             return new SuccessResult(Messages.Added);
         }
         public async Task<IResult> AddAsync(BrandAddDto brandAddDto)
         {
             var brand = _mapper.Map<Brand>(brandAddDto);
-            await _brandDal.AddAsync(brand);
+            await _brandRepository.AddAsync(brand);
             return new SuccessResult(Messages.Added);
         }
 
         public IResult Delete(BrandDto brandDto)
         {
             var brand = _mapper.Map<Brand>(brandDto);
-            _brandDal.Delete(brand);
+            _brandRepository.Delete(brand);
             return new SuccessResult(Messages.Deleted);
         }
 
         [CacheAspect]
         public IDataResult<List<BrandDto>> GetAll()
         {
-            var brands = _brandDal.GetAll();
+            var brands = _brandRepository.GetAll();
             var brandsDto = _mapper.Map<List<BrandDto>>(brands);
             return new SuccessDataResult<List<BrandDto>>(brandsDto);
         }
         public async Task<IDataResult<List<BrandDto>>> GetAllAsync()
         {
-            var brands = await _brandDal.GetAllAsync();
+            var brands = await _brandRepository.GetAllAsync();
             var brandsDto = _mapper.Map<List<BrandDto>>(brands);
             return new SuccessDataResult<List<BrandDto>>(brandsDto);
         }
         public IDataResult<BrandDto> GetById(int brandId)
         {
-            var brand = _brandDal.Get(b => b.Id == brandId);
+            var brand = _brandRepository.Get(b => b.Id == brandId);
             var brandDto = _mapper.Map<BrandDto>(brand);
             return new SuccessDataResult<BrandDto>(brandDto);
         }
         public async Task<IDataResult<BrandDto>> GetByIdAsync(int brandId)
         {
-            var brand = await _brandDal.GetAsync(x => x.Id == brandId);
+            var brand = await _brandRepository.GetAsync(x => x.Id == brandId);
             var brandDto = _mapper.Map<BrandDto>(brand);
             return new SuccessDataResult<BrandDto>(brandDto);
         }
@@ -85,7 +87,7 @@ namespace Business.Concrete
         public IResult Update(BrandUpdateDto brandDto)
         {
             var brand = _mapper.Map<Brand>(brandDto);
-            _brandDal.Update(brand);
+            _brandRepository.Update(brand);
             return new SuccessResult(Messages.Updated);
         }
 
@@ -93,7 +95,7 @@ namespace Business.Concrete
         //Business Rules
         public IResult CheckIfBrandNameExists(string brandName)
         {
-            var result = _brandDal.GetAll(b => b.Name == brandName).Any();
+            var result = _brandRepository.GetAll(b => b.Name == brandName).Any();
             if (result)
             {
                 return new ErrorResult();
@@ -105,8 +107,8 @@ namespace Business.Concrete
         [TransactionScopeAspect]
         public IResult TransactionalOperation(Brand brand)
         {
-            _brandDal.Update(brand);
-            _brandDal.Add(brand);
+            _brandRepository.Update(brand);
+            _brandRepository.Add(brand);
             return new SuccessResult(Messages.ProductUpdated);
         }
     }
